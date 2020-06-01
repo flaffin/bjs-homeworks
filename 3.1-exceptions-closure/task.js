ded0ca364639d295ba302e5b65ebae861117d95b
Anastasiya <flaffin@inbox.ru>
2020-06-01 21:28:07 +0300

    Выполнена 3 версия

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -8,6 +8,7 @@ function parseCount(numb){
     if (Number.isNaN(numNew)){
         throw new Error ("Невалидное значение");
     }
+   
     return numNew;    
 }
 
@@ -23,168 +24,61 @@ function validateCount(numbCount){
 
 }
 
-// проверки
-parseCount("123");
-// 123
-parseCount("012");
-// 12
- 
-parseCount("ыфва");
-// "Невалидное значение"
-
- validateCount("56");
-//  56
-
- validateCount("ыфва");
-//  Error: Невалидное значение
-
 
 // Задача 2
 
 class Triangle{
-    constructor(a, b, c){
+      constructor (a, b, c){
         this.a = a;
         this.b = b;
         this.c = c;
-    }
-    toBeDefined(){
-        let triangleFined;
-        try { 
+        this.triangle;
+      }
+        
+      get triangle() {
         if (((this.a + this.b) > this.c) && ((this.a + this.c) > this.b) && ((this.c + this.b) > this.a)) {
-           triangleFined = 'true';
+    
+     
+        return ; 
         } 
         else {
-            throw new Error ( "Треугольник с такими сторонами не существует");
+        throw new Error ("Треугольник с такими сторонами не существует");
         }
-        }
-        catch (err) {
-          return err;
-          // console.log("Треугольник с такими сторонами не существует");
-        }
-        return triangleFined;
-    }
+        
+      }
 
-  // первый вариант.  getPerimeter () Запускает метод toBeDefined(), для проверки существования треугольника. 
-    // Если треугольник не существует, то при вызове метода  getPerimeter ()  выдает ошибку и из toBeDefined() и свою ошибку P = "Ошибка! Неправильный треугольник"
-    getPerimeter (){
+
+      getPerimeter(){
         let P;
-
-       if (this.toBeDefined() === 'true'){
-         P = this.a + this.b + this.c;
-       }
-       else {
-        P = "Ошибка! Неправильный треугольник";
-       }
-    return P;
-
-    }
-
-    // второй вариант. getPerimeter () в себе производит проверку на существование треугольника.
-    // getPerimeter (){
-    //     let P;
-
-    //     if (((this.a + this.b) > this.c) && ((this.a + this.c) > this.b) && ((this.c + this.b) > this.a)) {
-    //      P = this.a + this.b + this.c;
-    //    }
-    //    else {
-    //     P = "Ошибка! Неправильный треугольник";
-    //    }
-    // return P;
-
-    // }
-
-    // первый вариант. getArea() Запускает метод toBeDefined(), для проверки существования треугольника. 
-    // Если треугольник не существует, то при вызове метода getArea()  выдает ошибку и из toBeDefined() и свою ошибку S = "Ошибка! Неправильный треугольник"
-
-    getArea (){
-    let S;
-
-      if (this.toBeDefined() === 'true'){
-         let p = (this.a + this.b + this.c) / 2 ;
-         S = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
-         S = S.toFixed(3);
+        P = this.a + this.b + this.c;
+        return P;
       }
-      else {
-       S = "Ошибка! Неправильный треугольник";
+
+      getArea(){
+        let S;
+        let p = (this.a + this.b + this.c) / 2 ;
+        S = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        S = Number(S.toFixed(3));
+        return S;
       }
-   return S;
 }
 
-// // второй вариант. getArea () в себе производит проверку на существование треугольника.
 
-//   getArea (){
-//     let S;
-
-//      if (((this.a + this.b) > this.c) && ((this.a + this.c) > this.b) && ((this.c + this.b) > this.a)) {
-
-//      let p = (this.a + this.b + this.c) / 2 ;
-//          S = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
-//          S = S.toFixed(3);
-
-//      } 
-//      else {
-//      S = "Ошибка! Неправильный треугольник";
-//      }
-//   return S;
-// }
-}
 
 function getTriangle(a, b, c) {
-  let newTrangl;
-  
- if (newTrangl = new Triangle(a,b,c)){
+  try {
+     const tria = new Triangle(a, b, c);
+     console.log(tria);
+     return tria;
 
-   if (newTrangl.toBeDefined() === 'true' ){
+  } catch (err) {
 
-    const triangle = new Triangle(a,b,c);
-
-     
-    //  console.log(triangle);
-
-   } 
-   else {
-    console.log(newTrangl.getArea());
-    console.log(newTrangl.getPerimeter());
-
-  //  return {
-    // newTrangl.getArea());
-    // console.log(newTrangl.getPerimeter());
-  //  };
-
-   }
+const errTria = {
+getArea: ()=> "Ошибка! Неправильный треугольник",
+getPerimeter: ()=> "Ошибка! Неправильный треугольник"
 }
- 
-return triangle;
-} 
-   
-   
-   
 
-//  Проверки
- getTriangle(1,3,100);
-
-//  Ошибка! Неправильный треугольник
-//  Ошибка! Неправильный треугольник
-
- getTriangle(100,3,10);
-
-//  Ошибка! Неправильный треугольник
-//  Ошибка! Неправильный треугольник
-
- getTriangle(6,10,15);
-//31
-// 20.123
-
- getTriangle(2,5,5);
-// 12
-// 4.899
-
-const triangle = new Triangle(1,3,100);
-console.log(triangle.toBeDefined());
-// Error: Треугольник с такими сторонами не существует
-//     at Triangle.toBeDefined (<anonymous>:14:19)
-//     at <anonymous>:2:22
-
-const triangle = new Triangle(6,10,15);
-console.log(triangle.toBeDefined());
-
+return errTria;
+}
+    
+}
